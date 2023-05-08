27ef6c2cdd2d555a4ee10e4064e29268520158ca
Gab4224 <gabluizberto@gmail.com>
2023-05-08 14:28:18 -0300

    oi

---
+++ src/main/resources/META-INF/resources/js/principal.js
@@ -0,0 +1,10 @@
+function irbanho(){
+    window.location.href = window.location.origin+"/banho";
+}
+function irtosa(){
+    window.location.href = window.location.origin+"/tosa";
+}
+function irprodutos(){
+    window.location.href = window.location.origin+"/produtos";
+}
+
